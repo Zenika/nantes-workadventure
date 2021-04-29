4f3434e451af21f16c5a55dae7ebb08fd21b4051
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-04-29 21:54:47 +0200

    Secret LB room (#4)

--- json-format.js
+++ json-format.js
@@ -2,7 +2,9 @@ const { readFile, writeFile } = require('fs/promises')
 const { resolve } = require('path');
 
 (async () => {
-  const path = resolve(process.argv[2])
-  const buffer = await readFile(path)
-  await writeFile(path, JSON.stringify(JSON.parse(buffer.toString()), undefined, '  '))
+  for (const arg of process.argv.slice(2)) {
+    const path = resolve(arg)
+    const buffer = await readFile(path)
+    await writeFile(path, JSON.stringify(JSON.parse(buffer.toString()), undefined, '  '))
+  }
 })()
